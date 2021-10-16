66005dac00935830362a3f98c4beb9651ec41519
dinesh aher <aher.dinesh56@gmail.com>
2021-10-16 14:34:31 +0530

    1. empty string return 0
    2. "1,2" return 3
    
    3. "1\n2,3" return 6

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -29,4 +29,35 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(6, stringCalculator.add("1\n2,3"));
     }
+    
+    
+    @Test
+    public void  string_with_NegativeNumber(){
+    	StringCalculator stringCalculator = new StringCalculator();
+    	try {
+    		stringCalculator.add("-1,2");
+		}
+		catch (IllegalArgumentException e){
+			assertEquals(e.getMessage(), "Negatives not allowed: -1");
+		}
+
+		try {
+			stringCalculator.add("2,-4,3,-5");
+		}
+		catch (IllegalArgumentException e){
+			assertEquals(e.getMessage(), "Negatives not allowed: -4,-5");
+		}
+    }
+    
+    @Test
+    public void testOverThousand(){
+    	StringCalculator stringCalculator = new StringCalculator();
+    	assertEquals(2, stringCalculator.add("1000,2"));
+    }
+
+    @Test
+    public void testOtherDelimiter(){
+    	StringCalculator stringCalculator = new StringCalculator();
+    	assertEquals(3, stringCalculator.add("//;\n1;2"));
+    }
 }
